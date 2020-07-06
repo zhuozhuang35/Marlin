797afc0633c5ce2e3108a6e8e2f2ed7a62bbedec
zhuozhuang35 <Charliezm88@gmail.com>
2020-07-06 16:57:29 +0800

    Movement Setting

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -782,7 +782,7 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
 
